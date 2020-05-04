e993c90e3e321b53fdc23651e5f8521d97bd3293
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-05-04 10:23:49 +0700

    custom alert untuk form

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -19,7 +19,17 @@ class ProdukController extends Controller
             'harga' => ' required|numeric',
             'stok' => 'required|numeric',
             'kondisi' => 'required|in:Y,N',
-            'deskripsi' => 'required',
-        ]);
+            'deskripsi' => 'required'],
+                [
+                    'nama_produk.required' => 'Nama produk harus diisi',
+                    'kategori.required' => 'Kategori harus diisi',
+                    'harga.required' => 'Harga harus diisi',
+                    'harga.numeric' => 'Harga harus berupa angka',
+                    'stok.required' => 'Kategori harus diisi',
+                    'stok.numeric' => 'Stok harus berupa angka',
+                    'kondisi.required' => 'Kondisi harus diisi',
+                    'kondisi.in' => 'Kondisi hanya baru dan bekas',
+                    'deskripsi.require' => 'Deskripsi harus diisi'
+                ]);
     }
 }
